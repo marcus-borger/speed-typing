5423cd8b3a8861f1eb4a161340e9431641c58aa6
marcus-borger <marcusborger@gmx.de>
2022-11-22 19:42:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
 const quoteContainer = document.querySelector("#quote-display");
 const quoteInput = document.querySelector("#quote-input");
 const timer = document.querySelector("#timer");
